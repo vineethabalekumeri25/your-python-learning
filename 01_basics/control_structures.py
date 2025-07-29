5938424d2c3f5405ee3de9bdd4c8611b0f7c52a2
vineethabalekumeri25 <vineethab322@gmail.com>
2025-07-29 11:14:35 +0530

    Added code for basics program

---
+++ 01_basics/control_structures.py
@@ -0,0 +1,19 @@
+# 01_basics/control_structures.py
+# Demonstrating basic control structures
+x = 10
+
+# if-else
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is 5 or less")
+
+# for loop
+for i in range(3):
+    print("Iteration", i)
+
+# while loop
+count = 0
+while count < 3:
+    print("Count:", count)
+    count += 1
---
+++ 01_basics/variables.py
@@ -0,0 +1,11 @@
+# 01_basics/variables.py
+# Demonstrating variable declaration and usage
+name = "Alice"
+age = 25
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student:", is_student)
