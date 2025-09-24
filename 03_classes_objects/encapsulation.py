28333b8d0f4b32ecc275b030d2ab379ddd040498
vineethabalekumeri25 <vineethab322@gmail.com>
2025-09-24 10:57:29 +0530

    Updated encapsulation.py file

--- 03_classes_objects/encapsulation.py
+++ 03_classes_objects/encapsulation.py
@@ -6,9 +6,16 @@ class BankAccount:
     def deposit(self, amount):
         self.__balance += amount
 
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("Insufficient funds")
+
     def get_balance(self):
         return self.__balance
 
 account = BankAccount(1000)
 account.deposit(500)
 print(account.get_balance())
+account.withdraw(2000)       # Prints "Insufficient funds"
