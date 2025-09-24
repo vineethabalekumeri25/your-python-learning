171cd85d9ced597db3f7e1d7cb2fb916b9c17b20
vineethabalekumeri25 <vineethab322@gmail.com>
2025-09-24 10:57:08 +0530

    Updated class_definition.py file

--- 03_classes_objects/class_definition.py
+++ 03_classes_objects/class_definition.py
@@ -7,5 +7,8 @@ class Person:
     def greet(self):
         print(f"Hi, I am {self.name} and I am {self.age} years old.")
 
-p = Person("Alice", 25)
-p.greet()
+p1 = Person("Alice", 25)
+p2 = Person("Bob", 24)
+
+p1.greet()
+p2.greet()
