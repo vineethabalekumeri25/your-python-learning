b83d051ab8156c51b83039c185e7e6049e6145db
vineethabalekumeri25 <vineethab322@gmail.com>
2025-09-25 15:01:18 +0530

    Updated creating_modules.py file

--- 04_enums_and_modules/creating_modules.py
+++ 04_enums_and_modules/creating_modules.py
@@ -1,10 +1,5 @@
-# 04_enums_and_modules/creating_modules.py
-# A simple module with utility functions
-def square(n):
-    return n * n
+# main.py (importing the module)
+import greetings
 
-def cube(n):
-    return n * n * n
-
-print("Square of 3:", square(3))
-print("Cube of 2:", cube(2))
+print(greetings.say_hello("Alice"))
+print(greetings.say_goodbye("Bob"))
